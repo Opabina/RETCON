715a6a0cb8ef5d8018b675c5ec25ac246b462d32
AveryOcean <averytoad65@gmail.com>
2022-10-22 00:47:28 +0200

    Remove C++ Project

--- C++ (DEPRECATED)/RETCON-Core/src/RETCON/Window/Window.cpp
+++
@@ -1,9 +0,0 @@
-#include <pch.h>
-#include "Window.h"
-
-namespace RETCON {
-	Window::Window(const std::string& name, const int& width, const int& height)
-		: m_WindowName(name), m_WindowWidth(width), m_WindowHeight(height)
-	{
-	}
-}
--- C++ (DEPRECATED)/Testing/src/TestApp.cpp
+++
@@ -1,8 +0,0 @@
-#include "RETCON.h"
-
-class Test : public RETCON::Application
-{};
-
-RETCON::Application* RETCON::CreateApplication() {
-	return new Test();
-}
