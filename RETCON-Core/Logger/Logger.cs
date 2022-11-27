dfce3d73cb97029adcfe22b6ccf258ecaf9940c8
AveryOcean <averytoad65@gmail.com>
2022-11-27 18:17:09 +0100

    Add Project Back with changes stashed on main

---
+++ RETCON-Core/Logger/Logger.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using RETCON.Core.Utils;
+
+namespace RETCON.Core.Logger
+{
+    public class Logger
+    {
+        public static Logger Engine = new Logger("Engine");
+        public static Logger Application = new Logger("Application");
+
+        private string _name;
+        private int _logCount;
+
+        private ConsoleColor _defaultForeground;
+        private ConsoleColor _defaultBackground;
+
+        public Logger(string name)
+        {
+            _name = name;
+            _defaultForeground = Console.ForegroundColor;
+            _defaultBackground = Console.BackgroundColor;
+        }
+
+        ~Logger()
+        {
+
+        }
+
+        private static bool _prevNewline = true;
+        public void Log(string text, ConsoleColor fg, ConsoleColor bg = ConsoleColor.Black, bool newline = true)
+        {
+            TextUtils.ChangeConsoleColor(fg, bg);
+         
+            // Create output string
+            var output = _prevNewline ? $"[{_name}]: {text}" : $"{text}";
+            
+            if(newline)
+            {
+                output += "\n";
+                _prevNewline = true;
+            } else
+            {
+                _prevNewline = false;
+            }
+
+            // Write to output handle
+            Console.Write(output);
+
+            // Post-write
+            TextUtils.ChangeConsoleColor(_defaultForeground, _defaultBackground);
+            
+            _logCount++;
+        }
+
+        public int GetLogCount()
+        {
+            return _logCount;
+        }
+
+        public void Assert(bool assert, string success = "Assertion Successful!", string fail = "Assertion Failed!")
+        {
+            if (assert)
+            {
+                Log(success, LogColors.Success);
+                return;
+            }
+
+            Log(fail, LogColors.Error);
+        }
+
+        public override string ToString()
+        {
+            return $"[{_name}]";
+        }
+    }
+
+    public struct LogColors
+    {
+        public static ConsoleColor Critical = ConsoleColor.DarkRed;
+        public static ConsoleColor Error = ConsoleColor.Red;
+        public static ConsoleColor Warning = ConsoleColor.Yellow;
+        public static ConsoleColor Trace = ConsoleColor.White;
+        public static ConsoleColor Success = ConsoleColor.Green;
+    }
+}
---
+++ RETCON-Core/Logger/TextUtils.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace RETCON.Core.Utils
+{
+    public static class TextUtils
+    {
+        public static void ChangeConsoleColor(ConsoleColor fg = ConsoleColor.White, ConsoleColor bg = ConsoleColor.Black)
+        {
+            Console.ForegroundColor = fg;
+            Console.BackgroundColor = bg;
+        }
+    }
+}
